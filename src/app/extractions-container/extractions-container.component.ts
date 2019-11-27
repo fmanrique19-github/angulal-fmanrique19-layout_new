8a4335a3f8ec08feae17fa9138a911d75c9ac993
fmanrique19-github <55700436+fmanrique19-github@users.noreply.github.com>
2019-11-27 08:45:58 -0500

    test

--- src/app/extractions-container/extractions-container.component.ts
+++ src/app/extractions-container/extractions-container.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
-import { ExtractionService } from "@app/providers/extraction.service";
+import { ExtractionService } from "app/providers/extraction.service";
 import { MatSort } from "@angular/material/sort";
 import { MatTableDataSource } from "@angular/material/table";
 
